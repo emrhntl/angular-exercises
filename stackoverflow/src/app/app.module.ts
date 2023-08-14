d075d6a2b069f8374e1706461d278f30593313fc
Emirhan <emirhanetli2003@gmail.com>
2023-08-14 12:34:46 +0300

    added signup and login screens

--- stackoverflow/src/app/app.module.ts
+++ stackoverflow/src/app/app.module.ts
@@ -4,21 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ stackoverflow/src/app/create-user/create-user.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent {
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  signupForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  })
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.signupForm.controls;
+  }
+
+}
